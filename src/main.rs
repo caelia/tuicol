69bb74e11ffdbb0f062148f6aa09e05a0867a322
Caelia R Chapin <crc@caelia.net>
2025-03-18 08:27:51 -0600

    Created a toy example that plays a sine wave.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+mod glikol;
+
+use glikol::GlicolWrapper;
+use cpal::{Host, HostId, host_from_id};
+use cpal::traits::HostTrait;
+use rodio::OutputStream;
+use std::time::Duration;
+use std::thread;
+// use std::Iter;
+
 fn main() {
-    println!("Hello, world!");
+    let host = host_from_id(HostId::Jack).unwrap();
+    let device = host.default_output_device().unwrap();
+    let mut wrapper = GlicolWrapper::new();
+    wrapper.eval(r#"o: sin 440"#);
+    let (_stream, handle) = OutputStream::try_from_device(&device).unwrap();
+    // let (_stream, handle) = OutputStream::try_default().unwrap();
+    let _ = handle.play_raw(wrapper);
+    thread::sleep(Duration::from_millis(1500));
 }
