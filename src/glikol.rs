8da2d9af3b618272cfb0255aaa53f11f4a7db1be
Caelia R Chapin <crc@caelia.net>
2025-03-19 18:27:01 -0600

    Still not working ...

--- src/glikol.rs
+++ src/glikol.rs
@@ -9,6 +9,11 @@ use std::time::Duration;
 use std::sync::{Arc, Mutex};
 // use std::cell::{RefCell, RefMut};
 
+pub fn glicol_wrapper() -> Arc<Mutex<GlicolWrapper>> {
+    Arc::new(Mutex::new(GlicolWrapper::new()))
+}
+
+#[derive(Clone)]
 enum Channel {
     L,
     R
@@ -28,17 +33,28 @@ pub enum Message {
     Process(&'static str)
 }
 
-pub struct GlicolWrapper {
-    engine: Arc<Mutex<glicol::Engine<32>>>,
-    channel: Channel,
-    data_l: VecDeque<f32>,
-    data_r: VecDeque<f32>
+pub struct GlicolWrapper<'a> {
+    engine: &'a mut glicol::Engine<32>,
+    channel: &'a Channel,
+    data_l: &'a VecDeque<f32>,
+    data_r: &'a VecDeque<f32>
+}
+
+impl<'a> Clone for GlicolWrapper<'a> {
+    fn clone(&self) -> Self {
+        GlicolWrapper {
+            engine: &self.engine.borrow_mut(),
+            channel: &self.channel.clone(),
+            data_l: &self.data_l.clone(),
+            data_r: &self.data_r.clone()
+        }
+    }
 }
 
 impl GlicolWrapper {
     pub fn new() -> Self {
         GlicolWrapper {
-            engine: Arc::new(Mutex::new(Engine::<32>::new())),
+            engine: Engine::<32>::new(),
             channel: Channel::L,
             data_l: VecDeque::new(),
             data_r: VecDeque::new()
@@ -46,26 +62,17 @@ impl GlicolWrapper {
     }
 
     pub fn eval(&mut self, code: &str) {
-        // self.engine.update_with_code(code);
-        if let Some(eng_mtx) = Arc::get_mut(&mut self.engine) {
-            let eng = eng_mtx.get_mut().unwrap();
-            eng.update_with_code(code);
-        }
+        self.engine.update_with_code(code);
     }
 
     fn update(&mut self) -> bool {
-        if let Some(eng_mtx) = Arc::get_mut(&mut self.engine) {
-            let eng = eng_mtx.get_mut().unwrap();
-            let (bufs, _mystery_data) = eng.next_block(vec![]);
-            if bufs[0].is_empty() || bufs[1].is_empty() {
-                false
-            } else {
-                self.data_l = VecDeque::from(bufs[0].to_vec());
-                self.data_r = VecDeque::from(bufs[1].to_vec());
-                true
-            }
-        } else {
+        let (bufs, _mystery_data) = self.engine.next_block(vec![]);
+        if bufs[0].is_empty() || bufs[1].is_empty() {
             false
+        } else {
+            self.data_l = VecDeque::from(bufs[0].to_vec());
+            self.data_r = VecDeque::from(bufs[1].to_vec());
+            true
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,42 @@
 
 mod glikol;
 
-use glikol::GlicolWrapper;
+use glikol::glicol_wrapper;
 use cpal::{Host, HostId, host_from_id};
 use cpal::traits::HostTrait;
 use rodio::OutputStream;
 use std::time::Duration;
 use std::thread;
-// use std::Iter;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     let host = host_from_id(HostId::Jack).unwrap();
     let device = host.default_output_device().unwrap();
-    let mut wrapper = GlicolWrapper::new();
-    wrapper.eval(r#"o: sin 220"#);
-    /*
-    wrapper.eval(r#"
-        ~gate: speed 2.0
-        >> seq 60 _60 _~a 48;
-        ~a: choose 48 48 48 72 0 0 0
-        ~amp: ~gate >> envperc 0.001 0.1;
-        ~pit: ~gate
-        >> mul ##Math.pow(2, (60-69)/12) * 440#
-        // mix js to get 261.63
-        ~lead: saw ~pit >> mul ~amp >> lpf ~mod 5.0
-        >> meta `
-            output = input.map(|x|x*0.1);
-            output
-        ` // rhai script, same as "mul 0.1"
-        ~mod: sin 0.2 >> mul 1300 >> add 1500;
-        out: ~lead >> add ~drum >> plate 0.1 // optinal semicolon
-        ~drum: speed 4.0 >> seq 60 >> sp \808bd;
-        "#);
-        */
     let (_stream, handle) = OutputStream::try_from_device(&device).unwrap();
-    // let (_stream, handle) = OutputStream::try_default().unwrap();
-    let _ = handle.play_raw(wrapper);
-    thread::sleep(Duration::from_millis(1500));
+    let mut wrapper = glicol_wrapper();
+    match Arc::get_mut(&mut wrapper) {
+        Some(mtx) => {
+            match mtx.get_mut() {
+                Ok(wrpr) => {
+                    wrpr.eval(r#"o: sin 220"#);
+                },
+                _ => ()
+            }
+        },
+        None => ()
+    }
+    match Arc::get_mut(&mut wrapper) {
+        Some(mtx) => {
+            match mtx.get_mut() {
+                Ok(wrpr) => {
+                    let _ = handle.play_raw(wrpr);
+                    thread::sleep(Duration::from_millis(1500));
+                },
+                _ => ()
+            }
+        },
+        None => ()
+    }
     /*
     wrapper.eval("");
     thread::sleep(Duration::from_millis(1500));
