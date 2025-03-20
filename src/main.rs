618fe3cf52ea874beb10e97d222d924024fc845a
Caelia R Chapin <crc@caelia.net>
2025-03-20 01:38:56 -0600

    We can now produce one sound, but not additional ones!

--- src/main.rs
+++ src/main.rs
@@ -24,25 +24,33 @@ fn main() {
     let (ctrl_req_tx, ctrl_req_rx) = channel();
     let (ctrl_rsp_tx, ctrl_rsp_rx) = channel();
         
+    let (tx, _rx) = (ctrl_req_tx, ctrl_rsp_rx);
     let mut wrapper = GlicolWrapper::new(data_rsp_tx, data_req_rx, ctrl_rsp_tx, ctrl_req_rx);
+    let _ = tx.send(CtrlReq::Process(r#"o: sin 440"#));
     thread::spawn(move || {
         wrapper.run();
     });
 
     let src = GlicolAudioSource::new(data_req_tx, data_rsp_rx);
 
-    let (tx, rx) = (ctrl_req_tx, ctrl_rsp_rx);
+    // let (tx, rx) = (ctrl_req_tx, ctrl_rsp_rx);
 
     let host = host_from_id(HostId::Jack).unwrap();
     let device = host.default_output_device().unwrap();
     let (_stream, handle) = OutputStream::try_from_device(&device).unwrap();
 
-    let _ = tx.send(CtrlReq::Process(r#"o: sin 220"#));
-    thread::sleep(Duration::from_millis(100));
+    // let _ = tx.send(CtrlReq::Process(r#"o: sin 220"#));
+    // thread::sleep(Duration::from_millis(100));
     thread::spawn(move || {
         let _ = handle.play_raw(src);
     });
     thread::sleep(Duration::from_millis(1500));
+
+    let _ = tx.send(CtrlReq::Process(r#""#));
+    thread::sleep(Duration::from_millis(1500));
+    let _ = tx.send(CtrlReq::Process(r#"o: saw 220"#));
+    thread::sleep(Duration::from_millis(1500));
+
     let _ = tx.send(CtrlReq::Stop);
     /*
     wrapper.eval("");
